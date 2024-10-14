766a5188818d3c5d986ba1e3fbd04cc8ad57baa4
kenechi <nsudekenechi2@gmail.com>
2024-10-14 11:14:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './assets/App.css'
+import "./assets/index.css"
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl'>Hello</h1>
+    </>
+  )
+}
+
+export default App
