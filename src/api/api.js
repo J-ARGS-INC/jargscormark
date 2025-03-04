95e95c99712cf847f89005f320161d6e3fbd32a0
J-ARGS-INC <176674496+J-ARGS-INC@users.noreply.github.com>
2025-03-04 15:54:25 +0100

    fixing .env

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const globalUrl = env == import.meta.env.VITE_DEV_ENV ? "http://localhost:3001" : "https://jargscormark-backend.vercel.app";
+export const globalUrl = import.meta.env.VITE_DEV_ENV ? "http://localhost:3001" : "https://jargscormark-backend.vercel.app";
 
 export const getItemInDB = (url, config) => axios.get(globalUrl + url, config).then(res => res.data)
 export const createItemInDB = (url, data, config) => axios.post(globalUrl + url, data, config).then(res => res.data);
